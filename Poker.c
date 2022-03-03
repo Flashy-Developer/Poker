c41dd0f3e56a1d528e67311dca31a4bb0f91b658
oOBESTOo <100703747+oOBESTOo@users.noreply.github.com>
2022-03-03 16:40:32 +0700

    Update Poker.c

--- Poker.c
+++ Poker.c
@@ -3,18 +3,207 @@
 #include <stdlib.h>
 
 #ifdef _WIN32
-# define clear()    system("cls")
+#define clear()    system("cls")
 #else
-# define clear()    system("clear")
+#define clear()    system("clear")
 #endif
 
+int Playgame(void);
+int Shuffcard(int cards[]);
+void Piccard(int num);
+
+int main()
+{
+	clear();
+	Playgame();
+}
+
+int Playgame(void){
+	typedef struct{
+		int Pycards[5];
+		int psum;
+	}Player;
+	Player Py={{0},0};
+	typedef struct{
+		int Dlcards[5];
+		int bsum;
+	}Dealer;
+	Dealer Dl={{0},0};
+	int i;
+	int cards[52];
+	int Pok=0;
+	char j ;
+	Shuffcard(cards);
+	Py.Pycards[0]=cards[0];
+	Py.Pycards[1]=cards[1];
+	Py.Pycards[2]=cards[4];
+	Dl.Dlcards[0]=cards[2];
+	Dl.Dlcards[1]=cards[3];
+	Dl.Dlcards[2]=cards[5];
+	printf("Cards of Dealer : \n");
+		printf(" _______\n");
+		printf("|       |\n");
+		printf("|       |\n");
+		printf("|   ?   |\n");
+		printf("|       |\n");
+		printf("|       |\n");
+		printf("|_______|\n");
+		printf(" _______\n");
+		printf("|       |\n");
+		printf("|       |\n");
+		printf("|   ?   |\n");
+		printf("|       |\n");
+		printf("|       |\n");
+		printf("|_______|\n");		
+	printf("\n");
+	printf("Cards of player :\n");
+	Piccard(Py.Pycards[0]);
+	Piccard(Py.Pycards[1]);
+	 i=0;
+	for (i=0;i<2;i++){
+		if(Py.Pycards[i]%100==11||Py.Pycards[i]%100==12||Py.Pycards[i]%100==13){
+		 Py.psum=Py.psum+0;
+		 }
+		else 
+		{
+			Py.psum=Py.psum+(Py.Pycards[i]%100);
+		}
+	}
+	if(Py.psum>9)
+	    Py.psum=Py.psum-10;
+	else if (Py.psum>=20)
+		Py.psum=Py.psum-20;
+	
+	for (i=0;i<2;i++){
+		if(Dl.Dlcards[i]%100==11||Dl.Dlcards[i]%100==12||Dl.Dlcards[i]%100==13){
+		 Dl.bsum=Dl.bsum+0;
+		 }
+		else {
+			Dl.bsum=Dl.bsum+(Dl.Dlcards[i]%100);
+		}
+	}
+	if(Dl.bsum>9)
+		Dl.bsum=Dl.bsum-10;
+	else if (Dl.bsum>=20)
+		Dl.bsum=Dl.bsum-20;
+	printf("Your score is : %d\n",Py.psum);	
+	if (Dl.bsum>7||Py.psum>7){
+		clear();
+		printf("Your card is \n");
+		Piccard(Py.Pycards[0]);
+	    Piccard(Py.Pycards[1]);
+		printf("Dealer card is \n");
+		Piccard(Dl.Dlcards[0]);
+	    Piccard(Dl.Dlcards[1]);
+		printf("Dealer score is : %d\n",Dl.bsum);
+		printf("Your score is : %d\n",Py.psum);
+		i=0;
+		for (i=8;i<=9;i++){
+			if (Dl.bsum==i){
+				printf("Dealer Poker %d\n",i);
+				if(Dl.bsum>Py.psum){
+					printf("!!!!Dealer Win!!!!\n");
+					Pok=1;
+				}
+			}
+			if (Py.psum==i){
+				printf("Player Poker %d\n",i);
+				if(Py.psum>Dl.bsum){
+					printf("!!!!Player Win!!!!\n");
+					Pok=1;
+					}
+			}
+			if (Py.psum==Dl.bsum){
+				printf("!!!!Draw!!!!\n");
+				Pok=1;
+			}
+		}
+	}
+	if(Pok==0){
+		printf("Want more cards? input y|n : ");
+		j='\0';
+		scanf("%c",&j);
+			printf("\nyour card 3 is  \n");
+			Piccard(Dl.Dlcards[2]);
+			if(Py.Pycards[2]%100==11||Py.Pycards[2]%100==12||Py.Pycards[2]%100==13){
+				Py.psum=Py.psum+0;
+				}
+			else {
+				Py.psum=Py.psum+(Py.Pycards[2]%100);
+			}
+			if(Py.psum>9)
+				Py.psum=Py.psum-10;
+			else if (Py.psum>=20)
+				Py.psum=Py.psum-20;
+			else if (Py.psum>=30)
+				Py.psum=Py.psum-30;
+			if(Dl.Dlcards[2]%100==11||Dl.Dlcards[2]%100==12||Dl.Dlcards[2]%100==13){
+				Dl.bsum=Dl.bsum+0;
+			}
+			else {
+				Dl.bsum=Dl.bsum+(Dl.Dlcards[2]%100);
+			}
+			if(Dl.bsum>9)
+				Dl.bsum=Dl.bsum-10;
+			else if (Dl.bsum>=20)
+				Dl.bsum=Dl.bsum-20;
+			else if (Dl.bsum>=30)
+				Dl.bsum=Dl.bsum-30;
+		    clear();
+			printf("Your card is \n");
+			Piccard(Py.Pycards[0]);
+	    	Piccard(Py.Pycards[1]);
+			Piccard(Py.Pycards[2]);
+			printf("Dealer card is \n");
+			Piccard(Dl.Dlcards[0]);
+	    	Piccard(Dl.Dlcards[1]);
+			Piccard(Dl.Dlcards[2]);
+			printf("Dealer score is : %d\n",Dl.bsum);
+			printf("Your score is : %d\n",Py.psum);
+			if(Dl.bsum>Py.psum)
+				{
+				printf("!!!!Dealer Win!!!!\n");
+				}
+			else if(Dl.bsum<Py.psum){
+				printf("!!!!Player Win!!!!\n");
+				}
+			else if (Py.psum==Dl.bsum){
+				printf("!!!!Draw!!!!\n");
+				}
+		else{
+			clear();
+			printf("Stand\n");
+			printf("Your card is \n");
+			Piccard(Py.Pycards[0]);
+	    	Piccard(Py.Pycards[1]);
+			printf("Dealer card is \n");
+			Piccard(Dl.Dlcards[0]);
+	    	Piccard(Dl.Dlcards[1]);
+			printf("Dealer score is : %d\n",Dl.bsum);
+			printf("Your score is : %d\n",Py.psum);
+			if(Dl.bsum>Py.psum){
+				printf("!!!!Dealer Win!!!!\n");
+			}
+			else if(Dl.bsum<Py.psum){
+				printf("!!!!Player Win!!!!\n");
+
+				}
+			else if (Py.psum==Dl.bsum){
+				printf("!!!!Draw!!!!\n");
+				}
+		}		
+	}
+	return 1;
+}
 int Shuffcard(int cards[])
 {
+	
 	int t;
 	int i;
 	int desk[52];	
 	for (i=0;i<52;i++)
 		desk[i] = (i/13+3)*100 + i%13 + 1;
+
 	srand(time(NULL));
 	for (i = 0; i < 52; i++)
 	{
@@ -25,7 +214,7 @@ int Shuffcard(int cards[])
 		cards[i] = desk[t];
 		desk[t] = 0;
 	}
-	
+
 	return 0;
 }
 
@@ -38,18 +227,10 @@ void Piccard(int num)
 	Pnum = num % 100;
 	switch (Pnum)
 	{
-		case 1:
-			printf(" _______\n");
-			printf("|A      |\n");
-			printf("|       |\n");
-			printf("|   %c   |\n",fl);
-			printf("|       |\n");
-			printf("|      A|\n");
-			printf("|_______|\n");
-			break;
-		case 2:
+		case 1: 	 	
+		case 2:		
 		case 3:
-		case 4:
+		case 4:		
 		case 5:
 		case 6:
 		case 7:
@@ -111,231 +292,4 @@ void Piccard(int num)
 		}
 
 	}
-}
-
-int Playgame(void){
-	int psum=0;
-	int bsum=0;
-	int Pycards[5]={0};
-	int Dlcards[5]={0};
-	int cards[52];
-	int Pok=0;
-	
-	Shuffcard(cards);
-
-	Pycards[0]=cards[0];
-	Pycards[1]=cards[1];
-	Pycards[2]=cards[4];
-	Dlcards[0]=cards[2];
-	Dlcards[1]=cards[3];
-	Dlcards[2]=cards[5];
-	
-	printf("Cards of Dealer : \n");
-		printf(" _______\n");
-		printf("|       |\n");
-		printf("|       |\n");
-		printf("|   ?   |\n");
-		printf("|       |\n");
-		printf("|       |\n");
-		printf("|_______|\n");
-		printf(" _______\n");
-		printf("|       |\n");
-		printf("|       |\n");
-		printf("|   ?   |\n");
-		printf("|       |\n");
-		printf("|       |\n");
-		printf("|_______|\n");		
-	printf("\n");
-	printf("Cards of player :\n");
-	Piccard(Pycards[0]);
-	Piccard(Pycards[1]);
-	int i;
-	for (i=0;i<2;i++){
-		if(Pycards[i]%100==11||Pycards[i]%100==12||Pycards[i]%100==13){
-		 psum=psum+0;
-		 }
-		else 
-		{
-			psum=psum+(Pycards[i]%100);
-		}
-	}
-	if(psum>9)
-	    psum=psum-10;
-	else if (psum>=20)
-		psum=psum-20;
-	
-	for (i=0;i<2;i++){
-		if(Dlcards[i]%100==11||Dlcards[i]%100==12||Dlcards[i]%100==13){
-		 bsum=bsum+0;
-		 }
-		else {
-			bsum=bsum+(Dlcards[i]%100);
-		}
-	}
-	if(bsum>9)
-		bsum=bsum-10;
-	else if (bsum>=20)
-		bsum=bsum-20;
-	printf("Your score is : %d\n",psum);	
-	if (bsum>7||psum>7){
-		clear();
-		printf("Your card is \n");
-		Piccard(Pycards[0]);
-	    Piccard(Pycards[1]);
-		printf("Dealer card is \n");
-		Piccard(Dlcards[0]);
-	    Piccard(Dlcards[1]);
-		printf("Dealer score is : %d\n",bsum);
-		printf("Your score is : %d\n",psum);
-		for (i=8;i<=9;i++){
-			if (bsum==i){
-				printf("Dealer Poker %d\n",i);
-				if(bsum>psum){
-					printf("██████╗░███████╗░█████╗░██╗░░░░░███████╗██████╗░░██╗░░░░░░░██╗██╗███╗░░██╗\n");
-					printf("██╔══██╗██╔════╝██╔══██╗██║░░░░░██╔════╝██╔══██╗░██║░░██╗░░██║██║████╗░██║\n");
-					printf("██║░░██║█████╗░░███████║██║░░░░░█████╗░░██████╔╝░╚██╗████╗██╔╝██║██╔██╗██║\n");
-					printf("██║░░██║██╔══╝░░██╔══██║██║░░░░░██╔══╝░░██╔══██╗░░████╔═████║░██║██║╚████║\n");
-					printf("██████╔╝███████╗██║░░██║███████╗███████╗██║░░██║░░╚██╔╝░╚██╔╝░██║██║░╚███║\n");
-					printf("╚═════╝░╚══════╝╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝\n");
-					Pok=1;
-				}
-			}
-			if (psum==i){
-				printf("Player Poker %d\n",i);
-				if(psum>bsum){
-					printf("██████╗░██╗░░░░░░█████╗░██╗░░░██╗███████╗██████╗░░██╗░░░░░░░██╗██╗███╗░░██╗\n");
-					printf("██╔══██╗██║░░░░░██╔══██╗╚██╗░██╔╝██╔════╝██╔══██╗░██║░░██╗░░██║██║████╗░██║\n");
-					printf("██████╔╝██║░░░░░███████║░╚████╔╝░█████╗░░██████╔╝░╚██╗████╗██╔╝██║██╔██╗██║\n");
-					printf("██╔═══╝░██║░░░░░██╔══██║░░╚██╔╝░░██╔══╝░░██╔══██╗░░████╔═████║░██║██║╚████║\n");
-					printf("██║░░░░░███████╗██║░░██║░░░██║░░░███████╗██║░░██║░░╚██╔╝░╚██╔╝░██║██║░╚███║\n");
-					printf("╚═╝░░░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝\n");
-					Pok=1;
-					}
-			}
-			if (psum==bsum){
-				printf("██████╗░██████╗░░█████╗░░██╗░░░░░░░██╗\n");
-				printf("██╔══██╗██╔══██╗██╔══██╗░██║░░██╗░░██║\n");
-				printf("██║░░██║██████╔╝███████║░╚██╗████╗██╔╝\n");
-				printf("██║░░██║██╔══██╗██╔══██║░░████╔═████║░\n");
-				printf("██████╔╝██║░░██║██║░░██║░░╚██╔╝░╚██╔╝░\n");
-				printf("╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░\n");
-				Pok=1;
-			}
-		}
-	}
-	if(Pok==0){
-		char j;
-		printf("Want more cards? input y|n : ");
-		do{
-			j = getchar();
-		} while (j!='y' &&j!='n');
-		i=0;
-		if(j=='y'){
-			printf("\nyour card 3 is  \n");
-			Piccard(Dlcards[2]);
-			if(Pycards[3]%100==11||Pycards[3]%100==12||Pycards[3]%100==13){
-				psum=psum+0;
-				}
-			else {
-				psum=psum+(Pycards[3]%100);
-			}
-			if(psum>9)
-				psum=psum-10;
-			else if (psum>=20)
-				psum=psum-20;
-			else if (psum>=30)
-				psum=psum-30;
-			if(Dlcards[3]%100==11||Dlcards[3]%100==12||Dlcards[3]%100==13){
-				bsum=bsum+0;
-			}
-			else {
-				bsum=bsum+(Dlcards[i]%100);
-			}
-			if(bsum>9)
-				bsum=bsum-10;
-			else if (bsum>=20)
-				bsum=bsum-20;
-			else if (bsum>=30)
-				bsum=bsum-30;
-		    clear();
-			printf("Your card is \n");
-			Piccard(Pycards[0]);
-	    	Piccard(Pycards[1]);
-			Piccard(Pycards[2]);
-			printf("Dealer card is \n");
-			Piccard(Dlcards[0]);
-	    	Piccard(Dlcards[1]);
-			Piccard(Dlcards[2]);
-			printf("Dealer score is : %d\n",bsum);
-			printf("Your score is : %d\n",psum);
-			if(bsum>psum)
-				{
-				printf("██████╗░███████╗░█████╗░██╗░░░░░███████╗██████╗░░██╗░░░░░░░██╗██╗███╗░░██╗\n");
-				printf("██╔══██╗██╔════╝██╔══██╗██║░░░░░██╔════╝██╔══██╗░██║░░██╗░░██║██║████╗░██║\n");
-				printf("██║░░██║█████╗░░███████║██║░░░░░█████╗░░██████╔╝░╚██╗████╗██╔╝██║██╔██╗██║\n");
-				printf("██║░░██║██╔══╝░░██╔══██║██║░░░░░██╔══╝░░██╔══██╗░░████╔═████║░██║██║╚████║\n");
-				printf("██████╔╝███████╗██║░░██║███████╗███████╗██║░░██║░░╚██╔╝░╚██╔╝░██║██║░╚███║\n");
-				printf("╚═════╝░╚══════╝╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝\n");
-				}
-			else if(bsum<psum){
-				printf("██████╗░██╗░░░░░░█████╗░██╗░░░██╗███████╗██████╗░░██╗░░░░░░░██╗██╗███╗░░██╗\n");
-				printf("██╔══██╗██║░░░░░██╔══██╗╚██╗░██╔╝██╔════╝██╔══██╗░██║░░██╗░░██║██║████╗░██║\n");
-				printf("██████╔╝██║░░░░░███████║░╚████╔╝░█████╗░░██████╔╝░╚██╗████╗██╔╝██║██╔██╗██║\n");
-				printf("██╔═══╝░██║░░░░░██╔══██║░░╚██╔╝░░██╔══╝░░██╔══██╗░░████╔═████║░██║██║╚████║\n");
-				printf("██║░░░░░███████╗██║░░██║░░░██║░░░███████╗██║░░██║░░╚██╔╝░╚██╔╝░██║██║░╚███║\n");
-				printf("╚═╝░░░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝\n");
-				}
-			else if (psum==bsum){
-				printf("██████╗░██████╗░░█████╗░░██╗░░░░░░░██╗\n");
-				printf("██╔══██╗██╔══██╗██╔══██╗░██║░░██╗░░██║\n");
-				printf("██║░░██║██████╔╝███████║░╚██╗████╗██╔╝\n");
-				printf("██║░░██║██╔══██╗██╔══██║░░████╔═████║░\n");
-				printf("██████╔╝██║░░██║██║░░██║░░╚██╔╝░╚██╔╝░\n");
-				printf("╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░\n");
-				}
-			
-		else{
-			clear();
-			printf("Stand\n");
-			printf("Your card is \n");
-			Piccard(Pycards[0]);
-	    	Piccard(Pycards[1]);
-			printf("Dealer card is \n");
-			Piccard(Dlcards[0]);
-	    	Piccard(Dlcards[1]);
-			printf("Dealer score is : %d\n",bsum);
-			printf("Your score is : %d\n",psum);
-			if(bsum>psum){
-				printf("██████╗░███████╗░█████╗░██╗░░░░░███████╗██████╗░░██╗░░░░░░░██╗██╗███╗░░██╗\n");
-				printf("██╔══██╗██╔════╝██╔══██╗██║░░░░░██╔════╝██╔══██╗░██║░░██╗░░██║██║████╗░██║\n");
-				printf("██║░░██║█████╗░░███████║██║░░░░░█████╗░░██████╔╝░╚██╗████╗██╔╝██║██╔██╗██║\n");
-				printf("██║░░██║██╔══╝░░██╔══██║██║░░░░░██╔══╝░░██╔══██╗░░████╔═████║░██║██║╚████║\n");
-				printf("██████╔╝███████╗██║░░██║███████╗███████╗██║░░██║░░╚██╔╝░╚██╔╝░██║██║░╚███║\n");
-				printf("╚═════╝░╚══════╝╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝\n");
-			}
-			else if(bsum<psum){
-				printf("██████╗░██╗░░░░░░█████╗░██╗░░░██╗███████╗██████╗░░██╗░░░░░░░██╗██╗███╗░░██╗\n");
-				printf("██╔══██╗██║░░░░░██╔══██╗╚██╗░██╔╝██╔════╝██╔══██╗░██║░░██╗░░██║██║████╗░██║\n");
-				printf("██████╔╝██║░░░░░███████║░╚████╔╝░█████╗░░██████╔╝░╚██╗████╗██╔╝██║██╔██╗██║\n");
-				printf("██╔═══╝░██║░░░░░██╔══██║░░╚██╔╝░░██╔══╝░░██╔══██╗░░████╔═████║░██║██║╚████║\n");
-				printf("██║░░░░░███████╗██║░░██║░░░██║░░░███████╗██║░░██║░░╚██╔╝░╚██╔╝░██║██║░╚███║\n");
-				printf("╚═╝░░░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝\n");
-				}
-			else if (psum==bsum){
-				printf("██████╗░██████╗░░█████╗░░██╗░░░░░░░██╗\n");
-				printf("██╔══██╗██╔══██╗██╔══██╗░██║░░██╗░░██║\n");
-				printf("██║░░██║██████╔╝███████║░╚██╗████╗██╔╝\n");
-				printf("██║░░██║██╔══██╗██╔══██║░░████╔═████║░\n");
-				printf("██████╔╝██║░░██║██║░░██║░░╚██╔╝░╚██╔╝░\n");
-				printf("╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░\n");
-				}
-		}
-	}
-	return 1;
-}
-
-int main()
-{
-	clear();
-	Playgame();
 }
